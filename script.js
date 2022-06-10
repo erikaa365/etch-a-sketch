e854515a3f314ad0eea0a33b1d48998d41964c64
erika <erika.milovec2@gmail.com>
2022-06-10 19:25:39 +0200

    Add page layout

---
+++ script.js
@@ -0,0 +1,26 @@
+const grid = document.getElementById('grid');
+let rows = document.getElementsByClassName('gridRow');
+let cells = document.getElementsByClassName('cell');
+
+function makeGrid(size){
+    makeRows(size);
+    makeColumns(size);
+}
+
+function makeRows(rowNum){
+    for(r = 0; r < rowNum; r++){
+        let row = document.createElement('div');
+        grid.appendChild(row).className = 'gridRow';
+    }
+}
+    
+function makeColumns(cellNum){
+    for(i = 0; i < rows.length; i++){
+        for(j = 0; j < cellNum; j++){
+            let newCell = document.createElement('div');
+            rows[j].appendChild(newCell).className = 'cell';
+        }
+    }
+}
+
+makeGrid(16);
